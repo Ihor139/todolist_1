8ef89bdf4088963c3c7f1adbbfee34dede10e7e5
Ihor139 <igor.radchuk.13@gmail.com>
2021-05-10 07:33:06 +0300

    js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
   $("[data-tab]").on("click", function () {
     console.log($('.todo-list-wrapper').find('.todo-list').data('tabContent', $(this).data('tab')));
-    
+    console.log(1421412512);
     console.log($(this).closest('.todo-menu-wrapper').siblings('.todo-list-wrapper')
     .find('.todo-list').data('tabContent' , $(this).data('tab')));
   });
