0a33311ee83e48d300c02c6b54b8fe7424328b14
ihor139 <igor.radchuk.13@gmail.com>
2021-05-10 17:56:51 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-$(document).ready(function () {
-    
-  
+$(document).ready(function() {
 
-  $("[data-tab]").on("click", function () {
-    // let tabs = $('.todo-list').data('tabContent' , $(this).data('tab'));
-    let tabs = document.querySelectorAll('.todo-list')
-    
-    console.log(tabs);
-    tabs.forEach(e => {
-      console.log($(e).data());
-      // showList()
+
+
+    $("[data-tab]").on("click", function() {
+        // let tabs = $('.todo-list').data('tabContent', $(this).data('tab'));
+        let tabs = document.querySelectorAll('[data-tab-content]')
+
+        console.log(tabs);
+        // tabs.forEach(e => {
+        //     console.log($(e).data());
+        // showList()
+        // });
+        // console.log($(this).closest('.todo-menu-wrapper').siblings('.todo-list-wrapper')
+        // .find('.todo-list').data('tabContent' , $(this).data('tab')));
+
     });
-    // console.log($(this).closest('.todo-menu-wrapper').siblings('.todo-list-wrapper')
-    // .find('.todo-list').data('tabContent' , $(this).data('tab')));
-    
-  });
+
+    /*$("[data-tab]").on("click", function(e) {
+        let tabCurrent = $(this).attr('data-tab');
+
+        $(document).find('[data-tab]').hide();
+        $(document).find('[data-tab-content=' + tabCurrent + ']').show();
+
+        //let content = tabCurrent;
+        //$(document).find('[data-tab-content]').children().remove();
+        //$(document).find('[data-tab-content]').append('<div class="content">' + content + '</div>');
+    });*/
 });
