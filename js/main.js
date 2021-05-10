c732d01f7580157f63250bafc9491e2baabe38a9
Ihor139 <igor.radchuk.13@gmail.com>
2021-05-10 08:31:57 +0300

    js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
   
 
   $("[data-tab]").on("click", function () {
-    let tabs = $('.todo-list').data('tabContent' , $(this).data('tab'));
-    let arr = Object.values(tabs);
+    // let tabs = $('.todo-list').data('tabContent' , $(this).data('tab'));
+    let tabs = document.querySelectorAll('.todo-list')
     
     console.log(tabs);
-    console.log(arr);
-    arr.forEach(e => {
-      console.log(e);
+    tabs.forEach(e => {
+      console.log($(e).data());
+      // showList()
     });
     // console.log($(this).closest('.todo-menu-wrapper').siblings('.todo-list-wrapper')
     // .find('.todo-list').data('tabContent' , $(this).data('tab')));
